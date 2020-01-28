223cbce57e473705247cb15fc1af8b23cea23358
hjälte <hjalte79@gmail.com>
2020-01-28 20:55:47 +0100

    Added Parameter property

--- ILogicRuleTester/ILogicHelpers.vb
+++ ILogicRuleTester/ILogicHelpers.vb
@@ -8,6 +8,28 @@ Module ILogicHelpers
     Public ThisDrawing As ICadDrawing = New CadDrawing(ThisApplication.ActiveDocument)
     Public ActiveSheet As ICadDrawingSheet = New CadDrawingSheet()
 
+    Public Property Parameter(name As String) As Object
+        Get
+            Return getParameter(name).Value
+        End Get
+        Set(value As Object)
+            getParameter(name).Expression = value
+        End Set
+    End Property
 
+    Private Function getParameter(name As String) As Parameter
+        Dim doc As Document = ThisDoc.Document
+        If (doc.DocumentType = DocumentTypeEnum.kPartDocumentObject) Then
+            Dim pDoc As PartDocument = doc
+            Dim def As PartComponentDefinition = pDoc.ComponentDefinition
+            Return def.Parameters.Item(name)
+        ElseIf (doc.DocumentType = DocumentTypeEnum.kAssemblyDocumentObject) Then
+            Dim pDoc As AssemblyDocument = doc
+            Dim def As AssemblyComponentDefinition = pDoc.ComponentDefinition
+            Return def.Parameters.Item(name)
+        Else
+            Throw New Exception("Cant get parameter. ThisDoc is not an part or assembly")
+        End If
+    End Function
 
 End Module
