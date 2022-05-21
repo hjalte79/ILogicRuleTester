70a380e909eac394461cb341594b58c8bfe47e94
Hjälte <hjalte79@users.noreply.github.com>
2022-05-21 22:04:25 +0200

    Removed forum post code

--- ILogicRuleTesterC_Core/ThisCRule.cs
+++ ILogicRuleTesterC_Core/ThisCRule.cs
@@ -11,22 +11,7 @@ namespace ILogicRuleTesterC_Core
     {
         public void Main()
         {
-            var _application = ThisApplication;
-            var path = @"D:\forum\2023\marked.ipt";
-
-
-            var oPositionMatrix = _application.TransientGeometry.CreateMatrix();
-            // Now create the component occurrence by calling the Add method of the ComponentOccurrences collection, referencing the part file previously created manually in Autodesk Inventor.
-            var assemblyDocument = (InventorInterop.AssemblyDocument)_application.ActiveDocument;
-            var invAssDocDef = assemblyDocument.ComponentDefinition;
-            var invAssOcc = invAssDocDef.Occurrences;
-            var debug1 = assemblyDocument.DisplayName;
-            var debug2 = invAssDocDef.ActivePositionalState;
-            var debug3 = invAssOcc.Count;
-            var translation = _application.TransientGeometry.CreateVector(2.0, 0.0, -1.0);
-            oPositionMatrix.SetTranslation(translation);
-            var debug4 = oPositionMatrix.Translation.Length;
-            invAssOcc.Add(path, oPositionMatrix);
+            // write your rule here
         }
     }
 }
